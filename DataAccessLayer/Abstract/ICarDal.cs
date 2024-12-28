36593c8ddbd4d05f6b0a78ed4995ed0037f4a5e0
Cugurel <cugurel7@gmail.com>
2024-12-28 11:39:35 +0300

    Car repository

---
+++ DataAccessLayer/Abstract/ICarDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICarDal
+    {
+        void AddCar(Car car);
+        void UpdateCar(Car car);
+        void DeleteCar(Car car);
+        Car GetById(int Id);
+        List<Car> GetAll();
+    }
+}
---
+++ DataAccessLayer/Concrete/EfRepository/EfCarRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EfRepository
+{
+    public class EfCarRepository : ICarDal
+    {
+        public void AddCar(Car car)
+        {
+            using var c = new Context();
+            c.Cars.Add(car);
+            c.SaveChanges();
+        }
+
+        public void DeleteCar(Car car)
+        {
+            using var c = new Context();
+            c.Cars.Remove(car);
+            c.SaveChanges();
+        }
+
+        public List<Car> GetAll()
+        {
+            using var c = new Context();
+            return c.Cars.ToList();
+        }
+
+        public Car GetById(int Id)
+        {
+            using var c = new Context();
+            return c.Cars.Find(Id);
+        }
+
+        public void UpdateCar(Car car)
+        {
+            using var c = new Context();
+            c.Cars.Update(car);
+            c.SaveChanges();
+        }
+    }
+}
