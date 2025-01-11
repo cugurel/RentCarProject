e265571ff8938072dcb88360685d0d2594cb9fd8
Cugurel <cugurel7@gmail.com>
2025-01-11 12:42:33 +0300

    IdentityContext

---
+++ UI/Models/Identity/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UI.Models.Identity
+{
+    public class ApplicationContext:IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
