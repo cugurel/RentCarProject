e9f0f742e2c2e62426fcc8fe1881a7f1d83c14b9
Cugurel <cugurel7@gmail.com>
2025-01-12 12:01:03 +0300

    Login html

---
+++ UI/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UI.Models.Identity;
+
+namespace UI.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Login(LoginModel model)
+		{
+			return View();
+		}
+	}
+}
