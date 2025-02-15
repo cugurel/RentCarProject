84393fcacacabbfc4c913867ddbcefe47ffd9701
Cugurel <cugurel7@gmail.com>
2025-02-15 10:38:13 +0300

    AddUser

--- UI/Controllers/UserController.cs
+++ UI/Controllers/UserController.cs
@@ -23,5 +23,47 @@ namespace UI.Controllers
             var users = _userManager.Users.ToList();
             return View(users);
         }
-    }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> AddUser(RegisterModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			var user = new User
+			{
+				FirstName = model.Firstname,
+				LastName = model.Surname,
+				Email = model.Email,
+				PhoneNumber = model.Phone,
+				UserName = model.Username
+			};
+
+
+			var userWithEmail = await _userManager.FindByEmailAsync(user.Email);
+
+			if (userWithEmail != null)
+			{
+				TempData["EmailError"] = "Bu email hesabı ile zaten bir kullanıcı var";
+				return View();
+			}
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (result.Succeeded)
+			{
+				TempData["SuccessRegister"] = "Kayıt başarılı!";
+				return RedirectToAction("Login", "Auth");
+			}
+			return View();
+		}
+	}
 }
