e87e423f2a057c523eb766aded4a5a1cab3e6cd2
Cugurel <cugurel7@gmail.com>
2025-03-01 11:25:19 +0300

    Role

---
+++ UI/Controllers/RoleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UI.Models.Identity;
+
+namespace UI.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+    }
+}
